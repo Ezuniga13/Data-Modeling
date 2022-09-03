7938467dba802e9ab839d7a9237a3053b9ffe076
Esteban Zuniga <60893597+Ezuniga13@users.noreply.github.com>
2022-09-03 00:02:04 -0400

    Update sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -38,7 +38,8 @@ songplay_table_insert = ("""INSERT INTO songplays (start_time, user_id, level, s
 """)
 
 user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level)
-                                    VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO NOTHING
+                                    VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) 
+                                    DO UPDATE SET level=EXCLUDED.level
 """)
 
 song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration)
